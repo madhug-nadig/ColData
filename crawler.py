ecd0aab4150b74d7f0acdb365bbd3e36015ffb7f
madhug-nadig <madhug.nadig@gmail.com>
2016-01-09 18:20:37 +0530

    With UNICODE

--- crawler.py
+++ crawler.py
@@ -2,11 +2,12 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 def getData(u):
-        r=urllib.request.urlopen(u)
-        soup=BeautifulSoup(r.read(),'html.parser')
+        html=urllib.request.urlopen(u)
+        data = html.read().decode('utf-8', 'ignore');
+        soup=BeautifulSoup(data)
         res = [i.text.replace('\n', ' ').strip() for i in soup.find_all('p')]
         for p in res:
                 print(p)
 
-url = 'http://fortune.com/2015/12/31/wall-street-market-performance-worries-year/';
+url = 'https://en.wikipedia.org/wiki/Adivasi';
 getData(url)
