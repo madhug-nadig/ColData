aee0f0a000dca367a0c535c2714138d6c145f538
madhug-nadig <madhug.nadig@gmail.com>
2016-01-17 09:29:46 +0530

    Data collector

--- crawler.py
+++ crawler.py
@@ -1,13 +1,20 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-def getData(u):
+def file_write(op):
+        file = open("data.txt", "w", encoding='utf-8')
+        file.write(op)
+        file.close()
+
+def get_data(u):
+        op = ""
         html=urllib.request.urlopen(u)
         data = html.read().decode('utf-8', 'ignore');
         soup=BeautifulSoup(data)
         res = [i.text.replace('\n', ' ').strip() for i in soup.find_all('p')]
         for p in res:
-                print(p)
+                op = op + p
+        file_write(op)
 
 url = 'https://en.wikipedia.org/wiki/Adivasi';
-getData(url)
+get_data(url)
